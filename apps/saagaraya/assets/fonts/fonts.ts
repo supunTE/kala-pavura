76754f4fc91beecec0e00d30e078f2414ce284cc
supunTE <supunedirisuriya2002@gmail.com>
2024-01-06 15:23:36 +0530

    feat(saagaraya): setup next app

---
+++ apps/saagaraya/assets/fonts/fonts.ts
@@ -0,0 +1,7 @@
+import {Noto_Serif_Sinhala} from "next/font/google"
+
+export const notoSerifSinhala = Noto_Serif_Sinhala({
+  subsets: ["sinhala"],
+  display: "swap",
+  weight: ['200', '400', '700']
+})
---
+++ apps/saagaraya/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
+module.exports = {
+  mode: 'jit',
+  presets: [require('../../tailwind-workspace-preset.js')],
+  content: [
+    join(__dirname, 'app/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
