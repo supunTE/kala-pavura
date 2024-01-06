c38b7757927700b1184f5464537fd8714aaa12ab
supunTE <supunedirisuriya2002@gmail.com>
2024-01-06 18:02:41 +0530

    Add pnpm workspace configuration and VS Code settings

--- apps/saagaraya/tailwind.config.js
+++ apps/saagaraya/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   presets: [require('../../tailwind-workspace-preset.js')],
   content: [
     join(__dirname, 'app/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
